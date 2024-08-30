7f6666334f0fbe8027ac9128a09e2887f734d951
Devendra Dhakal <devendradhakal@Devendras-MacBook-Pro.local>
2024-08-30 21:55:49 +1000

    added necessary dependencies and database connected

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectToMongoDb } from "./config/dbConfig.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// connect to database
+connectToMongoDb;
 
 // start our server
 app.listen(PORT, (error) => {
