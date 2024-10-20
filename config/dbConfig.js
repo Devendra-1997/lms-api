055838ff312a59cbb060fe8a8a3a790fb7da939f
Devendra-1997 <devendradhakal92@gmail.com>
2024-10-20 20:25:07 +1100

    deployed database in mongo atlas

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
+const DB_CONNECT_URL = process.env.DB_CONNECT_URL;
 
 export const connectToMongoDb = () => {
   try {
-    const connect = mongoose.connect(process.env.DB_CONNECT_URL + "/l-data");
+    const connect = mongoose.connect(DB_CONNECT_URL);
     if (connect) {
-      console.log(`Database conected: ${process.env.DB_CONNECT_URL}/l-data`);
+      console.log("Database conected");
     }
   } catch (error) {
     console.log("Error:", error);
